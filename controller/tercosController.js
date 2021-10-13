a87a9da883a948202f2a5ac597db9efe1bde7254
gleniodev <gleniodev@gmail.com>
2021-10-13 14:36:32 -0300

    path imagens dolorosos

--- controller/tercosController.js
+++ controller/tercosController.js
@@ -1,8 +1,17 @@
-const gloriosos = require('../database/gloriosos');
+const gloriosos = require('../database/gloriosos.json');
+const dolorosos = require('../database/dolorosos.json');
+const gozosos = require('../database/gozosos.json');
+const luminosos = require('../database/luminosos.json');
 
 const tercosController ={
     tercoMariano: (req, res, next) =>{
-        return res.render('tercoMariano', {tituloPage: "terço mariano", misterios: gloriosos});
+        return res.render('tercoMariano', {
+            tituloPage: "terço mariano", 
+            gloriosos: gloriosos,
+            dolorosos: dolorosos,
+            luminosos: luminosos,
+            gozosos: gozosos
+        });        
     }
 }
 
--- public/js/swiper.js
+++ public/js/swiper.js
@@ -26,19 +26,28 @@ const swiper2 = new Swiper('.swiper-2', {
 let cont = 0;
 let next = document.querySelector('#next');
 let prev = document.querySelector('#prev');
+let thumb = document.querySelector('#thumb');
 let slideAtual = document.querySelector('#slide-' + cont);
 let textoSlideAtual= "gloriosos";
+let textoSlideAtualSingular = "glorioso"
+
+let teste = document.createElement()
+
 
 next.addEventListener('click', () =>{
-    cont ++
-    let slideAtual = document.querySelector('#slide-' + cont)
-    textoSlideAtual = slideAtual.innerText    
-})
+    cont ++;
+    let slideAtual = document.querySelector('#slide-' + cont);
+    textoSlideAtual = slideAtual.innerText;    
+    textoSlideAtualSingular = textoSlideAtual.substring(0, textoSlideAtual.length -1).toLowerCase();
+    thumb.setAttribute('src', `<%= ${textoSlideAtual}.thumb %>`);
+    }
+)
 
 prev.addEventListener('click', () =>{
-    cont --
-    let slideAtual = document.querySelector('#slide-' + cont)
-    textoSlideAtual = slideAtual.innerText
+    cont --;
+    let slideAtual = document.querySelector('#slide-' + cont);
+    textoSlideAtual = slideAtual.innerText;
+    thumb.setAttribute('src', `<%= ${textoSlideAtual}.thumb %>`) ;
 })
 
 
