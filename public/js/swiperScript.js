bfe513f721ddd675978a26a6c50d5c5280ad9042
gleniodev <gleniodev@gmail.com>
2021-10-19 22:30:16 -0300

    scroll-snap finalizado

--- public/js/swiperScript.js
+++ public/js/swiperScript.js
@@ -1,5 +1,5 @@
 
-const swiper = new Swiper('.swiper', {     
+const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
@@ -12,26 +12,52 @@ const swiper = new Swiper('.swiper', {
 
     on: {
         slideChange: function () {
-          const index_currentSlide = swiper.realIndex;
-          const currentSlide = swiper.slides[index_currentSlide]
-          const targetData = currentSlide.getAttribute('data-target')
-          const titleData = currentSlide.getAttribute('data-title')
-          
-          const gloriosos = document.querySelector('.swiper-slide');
-          const dolorosos = document.getElementById('content-1');
-          const gozosos = document.getElementById('content-2');
-          const luminosos = document.getElementById('content-3');
-          
-          switch (titleData){
-              case 'dolorosos':
-                  gloriosos.style.display = 'none';
-                  gozosos.style.display = 'none';
-                  luminosos.style.display = 'none';
+            const index_currentSlide = swiper.realIndex;
+            const currentSlide = swiper.slides[index_currentSlide]
+            const targetData = currentSlide.getAttribute('data-target')
 
-          }
-          
+            const gloriosos = document.getElementById('wrapper-0');
+            const dolorosos = document.getElementById('wrapper-1');
+            const gozosos = document.getElementById('wrapper-2');
+            const luminosos = document.getElementById('wrapper-3');
+
+            switch (targetData) {
+                case 'wrapper-0':
+                    gloriosos.style.display = 'flex';
+                    dolorosos.style.display = 'none';
+                    gozosos.style.display = 'none';
+                    luminosos.style.display = 'none';
+                    gloriosos.scrollTo(0,0);
+                    break;
+
+                case 'wrapper-1':
+                    gloriosos.style.display = 'none';
+                    dolorosos.style.display = 'flex';
+                    gozosos.style.display = 'none';
+                    luminosos.style.display = 'none';
+                    dolorosos.scrollTo(0,0);
+                    break;
+
+                case 'wrapper-2':
+                    gloriosos.style.display = 'none';
+                    dolorosos.style.display = 'none';
+                    gozosos.style.display = 'flex';
+                    luminosos.style.display = 'none';
+                    gozosos.scrollTo(0,0);
+                    break;
+
+                case 'wrapper-3':
+                    gloriosos.style.display = 'none';
+                    gozosos.style.display = 'none';
+                    dolorosos.style.display = 'none';
+                    luminosos.style.display = 'flex';
+                    luminosos.scrollTo(0,0);
+                    break;
+
+            }
+
         },
-      },
+    },
 });
 
 const swiper2 = new Swiper('.swiper-2', {
