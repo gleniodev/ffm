602c3716f1b3eea4ddadeefd92a12d2ebefeb6bb
gleniodev <gleniodev@gmail.com>
2021-10-20 22:45:25 -0300

    manifest fullscren

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,1 @@
-import targetData from "./swiperScript";
-
-const pai = document.getElementById('swiper-wrapper');
-const buttonNext = document.getElementById('next');
-const luminosos = document.querySelector('#slide-3')
-
-luminosos.classList.add('swiper-active');
 
