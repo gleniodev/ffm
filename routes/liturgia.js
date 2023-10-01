ad5a8645e85b5ef86f3cfdc8205e9136618c5c76
gleniodev <gleniodev@gmail.com>
2023-10-01 11:28:34 -0300

    acerto da 2ª leitura, ajuste do min-heigth e scroll

--- routes/liturgia.js
+++ routes/liturgia.js
@@ -8,6 +8,8 @@ router.get('/primeiraLeitura', liturgiaController.primeiraLeitura);
 
 router.get('/salmo', liturgiaController.salmo);
 
+router.get('/segundaLeitura', liturgiaController.segundaLeitura);
+
 router.get('/evangelho', liturgiaController.evangelho);
 
 module.exports = router;
